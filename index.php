5506f84ab58efe47cc1a7fc09490d5d22a7118a0
Vinicius Borges Dev <dev.vinicius.borges@outlook.com>
2024-08-27 16:38:45 -0300

    Criação: componente navbar

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 if ($uri == '/') {
-    include './public/index.php';
+    require './public/index.php';
 }
 ?>
--- public/index.php
+++ public/index.php
@@ -6,11 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Titulo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="/public/assets/styles/index.css">
 </head>
 
 <body>
     <nav class="bg-alphacode d-flex align-items-center">
-        <img src="./assets/images/logo_alphacode.png" alt="logo alphacode">
+        <img src="/public/assets/images/logo_alphacode.png" alt="logo alphacode">
         <h1 class="text-white mx-4">Cadastro de contatos</h1>
     </nav>
 
