fc5a78e71209696a09a853a96080e37cc68687aa
Vinícius <dev.vinicius.borges@outlook.com>
2024-08-27 20:11:52 -0300

    Criação:
    classe controller que vai tratar das regras de negócio. Função de criação de contato foi inserido.

---
+++ src/controllers/contactController.php
@@ -0,0 +1,16 @@
+<?php
+require_once '../models/contactMoodel.php';
+
+class coontactController{
+
+
+    public function createContact(){
+        if ($_SERVER['REQUEST_METHOD']){
+            
+        };
+    };
+
+
+}
+
+?>
