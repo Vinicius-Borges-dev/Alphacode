488fef923b021dcb0aa557fe0132b66c6a1be1ec
Vinicius Borges Dev <dev.vinicius.borges@outlook.com>
2024-08-27 16:50:31 -0300

    Criação: componente de input padrão

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Titulo</title>
+    <title>Alphacode</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="/public/assets/styles/index.css">
 </head>
@@ -14,6 +14,18 @@
         <img src="/public/assets/images/logo_alphacode.png" alt="logo alphacode">
         <h1 class="text-white mx-4">Cadastro de contatos</h1>
     </nav>
+
+    <form class="container-fluid p-5">
+        <section class="row">
+            <section class="col-md-6">
+                <section class="mb-4" id="input-area">
+                    <label for="name alphacode" class="form-label fw-semibold">Nome completo</label>
+                    <input type="text" class="form-control alphacode-input rounded-0 p-0 py-2" id="name" name="name" placeholder="Ex.: Letícia Pacheco dos Santos" required>
+                </section>
+            </section>
+
+        </section>
+    </form>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
