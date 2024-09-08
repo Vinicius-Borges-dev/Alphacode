0cb97e7592408822eeefb2eb548606949ed3d44f
Dev-Vinicius-Borges <dev.vinicius.borges@outlook.com>
2024-09-08 00:33:38 -0300

    Modificação: Criação de método que renderiza views.

--- src/controllers/contactController.php
+++ src/controllers/contactController.php
@@ -5,4 +5,9 @@ require __DIR__ . '/../models/contactModel.php';
 class ContactController
 {
     private static $ContactModel = ContactModel::class;
+
+    public static function renderView($view, $data = [])
+    {   
+        include __DIR__ . "/../views/{$view}";
+    }
 }
