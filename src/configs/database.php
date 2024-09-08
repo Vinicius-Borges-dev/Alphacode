a3afb669b579d461cf9093bcba9c0adce59ec682
Dev-Vinicius-Borges <dev.vinicius.borges@outlook.com>
2024-09-08 00:19:34 -0300

    Modificação: Método construtor privado criado para seguir o modelo Singleton.

--- src/configs/database.php
+++ src/configs/database.php
@@ -9,6 +9,17 @@ class Database{
     private static $dsn;
     public static $conn;
 
+    private function __construct()
+    {
+        self::$dsn = 'mysql:dbname=' . self::$dbName . ';port=' . self::$port . ';host=' . self::$host;
+        try {
+            self::$conn = new PDO(self::$dsn, self::$username, self::$password);
+            self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $err) {
+            echo 'Erro no servidor: ' . $err->getMessage();
+        }
+    }
+
 }
 
 
