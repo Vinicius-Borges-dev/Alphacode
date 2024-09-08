96a3691e5cb7376177ede7060b684fc8f8c77021
Dev-Vinicius-Borges <dev.vinicius.borges@outlook.com>
2024-09-08 00:18:12 -0300

    Modificação: Modelo do Banco de dados foi alterado para ter atributos estáticos

--- src/configs/database.php
+++ src/configs/database.php
@@ -1,25 +1,13 @@
 <?php 
 
 class Database{
-    private $host = 'localhost';
-    private $database_name = 'alphacode';
-    private $username = 'root';
-    private $password = 'root';
-    private $port = 3306;
-    public $conn;
-
-    public function createConnection(){
-        $this->conn = null;
-        try{
-            $this->conn = new PDO("mysql:host=".$this->host.";port=". $this->port.";dbname=". $this->database_name, $this->username, $this->password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->conn->exec("set names utf8");
-        }catch(PDOException $err){
-            echo ("Erro na conexão ao banco de dados: ".$err->getMessage());
-        }
-
-        return $this->conn;
-    }
+    private static $host = "localhost";
+    private static $dbName = "alphacode";
+    private static $username = "root";
+    private static $password = "root";
+    private static $port = 3306;
+    private static $dsn;
+    public static $conn;
 
 }
 
