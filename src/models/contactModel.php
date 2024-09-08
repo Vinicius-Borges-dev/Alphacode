e4038dc39c73e6c40a14f6ebbf1a1f1a78ff234a
Dev-Vinicius-Borges <dev.vinicius.borges@outlook.com>
2024-09-08 00:30:47 -0300

    Modificação: Criação de todos os métodos estaticos de consulta ao banco de dados, todas as consultas são estaticas pois não dependem de instâncias.

--- src/models/contactModel.php
+++ src/models/contactModel.php
@@ -15,4 +15,48 @@ class ContactModel
     public static $receberSms;
     public static $receberEmail;
 
+    public static function createContact(){
+        $sql = 'INSERT INTO '.self::$table.' (nome, email, telefone, dataNascimento, profissao, celular, receberWhatsapp, receberSms, receberEmail) VALUES (?,?,?,?,?,?,?,?,?)';
+        $response = self::$conn::sqlQuery($sql, [self::$nome, self::$email, self::$telefone, self::$dataNascimento, self::$profissao, self::$celular, self::$receberWhatsapp, self::$receberSms, self::$receberEmail]);
+
+        if ($response->rowCount() > 0) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static function getAllContacts(){
+        $sql = 'SELECT * FROM '.self::$table;
+        return self::$conn::sqlQuery($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+    public static function getContactById($id){
+        $sql = 'SELECT * FROM '.self::$table.' WHERE id=?';
+        return self::$conn::sqlQuery($sql, [$id])->fetchAll(PDO::FETCH_ASSOC)[0];
+    }
+
+    public static function updateContact($id){
+        $sql = 'UPDATE '.self::$table.' SET nome=?, email=?, telefone=?, dataNascimento=?, profissao=?, celular=?, receberWhatsapp=?, receberSms=?, receberEmail=? WHERE id=?';
+        $response = self::$conn::sqlQuery($sql, [self::$nome, self::$email, self::$telefone, self::$dataNascimento, self::$profissao, self::$celular, self::$receberWhatsapp, self::$receberSms, self::$receberEmail, $id]);
+
+        if ($response->rowCount() > 0) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+    public static function deleteContactById($id){
+        $sql = 'DELETE FROM '.self::$table.' WHERE id=?';
+        $response = self::$conn::sqlQuery($sql, [$id]);
+
+        if ($response->rowCount() > 0) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
