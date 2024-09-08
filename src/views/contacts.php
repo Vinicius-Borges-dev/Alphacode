9916ba9c7c54b2262ecf2b1d01334ba0c68cb6f6
Dev-Vinicius-Borges <dev.vinicius.borges@outlook.com>
2024-09-08 00:43:32 -0300

    Modificação: Alterando a forma como a view contacts.php exibe os dados.

--- src/views/contacts.php
+++ src/views/contacts.php
@@ -1,6 +1,6 @@
 <?php
 
-foreach ($contatos as $contato) {
+foreach ($data as $contato) {
 ?>
 
     <tr>
@@ -15,10 +15,10 @@ foreach ($contatos as $contato) {
         <td class="fs-5 text-center text-nowrap p-4"><?= $contato['celular'] ?></td>
         <td class="text-center" colspan='2'>
             <span class="">
-                <button class="border border-0 bg-transparent" onclick="showUpdateForm(<?= $contato['id'] ?>)">
+                <button class="border border-0 bg-transparent" onclick="showUpdateForm(<?=$contato['id']?>)">
                     <img src="/public/assets/images/editar.png">
                 </button>
-                <button class="border border-0 bg-transparent" onclick="deleteContact(<?= $contato['id'] ?>)">
+                <button class="border border-0 bg-transparent" onclick="deleteContact(<?=$contato['id']?>)">
                     <img src="/public/assets/images/excluir.png">
                 </button>
             </span>
