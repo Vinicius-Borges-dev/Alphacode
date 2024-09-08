7b72e1892cf5842da9f5cba4aa5a91e915024073
Dev-Vinicius-Borges <dev.vinicius.borges@outlook.com>
2024-09-08 00:46:27 -0300

    Modificação: ALterando as funções dos serviços para serem padronizados e reutilizados.

--- public/assets/js/services/form.js
+++ public/assets/js/services/form.js
@@ -1,6 +1,30 @@
+async function sendFormContact(event, form, options = []) {
+    event.preventDefault();
+    $.ajax({
+        url: `${options instanceof Array ? `${form.method === 'post' ? `${options[0]}?contact=${options[1]}` : `${options[0]}/${options[1]}`}` : options}`,
+        type: `${form.method}`,
+        ContentType: 'application/json',
+        data: JSON.stringify({
+            nome: form.nome.value,
+            email: form.email.value,
+            telefone: form.telefone.value,
+            dataNascimento: form.dataNascimento.value,
+            profissao: form.profissao.value,
+            celular: form.celular.value,
+            receberWhatsapp: form.receberWhatsapp.checked,
+            receberSms: form.receberSms.checked,
+            receberEmail: form.receberEmail.checked,
+        }),
+        success: function () {
+            form.reset();
+            getContacts();
+        }
+    })
+}
+
 function getContacts() {
     $.ajax({
-        url: '/getContacts',
+        url: '/getAll',
         type: 'GET',
         success: function (data) {
             $('#contacts-list').html(data);
@@ -8,95 +32,28 @@ function getContacts() {
     })
 }
 
-function sendForm(e, form, Submit = []) {
-    e.preventDefault();
-    let id, path;
-    if (Submit instanceof Array) {
-        id = Submit[1]
-        path = Submit[0]
-    } else {
-        path = Submit
-    }
-    let formData = {
-        nome: form.nome.value,
-        email: form.email.value,
-        telefone: form.telefone.value,
-        dataNascimento: form.dataNascimento.value,
-        profissao: form.profissao.value,
-        celular: form.celular.value,
-        receberWhatsapp: form.receberWhatsapp.checked,
-        receberSms: form.receberSms.checked,
-        receberEmail: form.receberEmail.checked,
-    }
-    $.ajax({
-        url: `/${path}`,
-        type: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify({
-            id: id,
-            nome: formData.nome,
-            email: formData.email,
-            telefone: formData.telefone,
-            dataNascimento: formData.dataNascimento,
-            profissao: formData.profissao,
-            celular: formData.celular,
-            receberWhatsapp: formData.receberWhatsapp,
-            receberSms: formData.receberSms,
-            receberEmail: formData.receberEmail,
-        }),
-        success: function (data) {
-            form.reset();
-            console.log('Criado com sucesso');
-            getContacts();
-        },
-        error: function (err) {
-            console.log("Erro ao criar contato", err);
-        }
-    })
-}
-
 function showUpdateForm(id) {
     $.ajax({
-        url: '/edit',
-        type: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify({
-            id: id
-        }),
+        url: `/showUpdateForm?contact=${id}`,
+        type: 'GET',
         success: function (data) {
             $('#update-contact-form').html(data);
-
-            let modal = new bootstrap.Modal(document.getElementById('updateModal'));
+            let modal = new bootstrap.Modal('#updateModal')
             modal.show();
         }
     })
-
 }
 
-function deleteContact(id) {
+function deleteContact(id){
     $.ajax({
-        url: '/delete',
-        type: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify({
-            id: id
-        }),
+        url: `/delete?contact=${id}`,
+        type: 'GET',
         success: function () {
-            getContacts()
-        },
-        error: function (err) {
-            console.log('Erro ao deletar: ', err)
+            getContacts();
         }
     })
 }
 
-
-$(document).ready(function () {
+$(window).on('load', function () {
     getContacts();
-
-    $('input').on('focus', function () {
-        $(`label[for=${$(this).attr('id')}]`).addClass('text-alphacode')
-    }).on('blur', function () {
-        $(`label[for=${$(this).attr('id')}]`).removeClass('text-alphacode')
-    })
 })
